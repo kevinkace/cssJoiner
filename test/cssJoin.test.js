eab45dfeb03378b5a17149d2cc59195c6ff280db
kevinkace <kevinacameron@hotmail.com>
2018-05-04 08:50:54 -0700

    More tests

--- test/cssJoin.test.js
+++ test/cssJoin.test.js
@@ -13,6 +13,29 @@ test("no classes returns empty string", () => {
 });
 
 test("undefined returns empty string", () => {
-    var cssClass;
+    let cssClass;
+
     expect(cssJoin(cssClass)).toBe("");
+});
+
+test("if test is true, returns className", () => {
+    expect(cssJoin([true, "class1"])).toBe("class1");
+});
+
+test("if test is true, returns first className", () => {
+    expect(cssJoin([true, "class1", "class2"])).toBe("class1");
+});
+
+test("if test is false, returns empty string", () => {
+    expect(cssJoin([false, "class1"])).toBe("");
+});
+
+test("if test is false, returns second className", () => {
+    expect(cssJoin([false, "class1", "class2"])).toBe("class2");
+});
+
+test("if test param is false, returns first className", () => {
+    let test;
+
+    expect(cssJoin([!test, "class1"])).toBe("class1");
 });
